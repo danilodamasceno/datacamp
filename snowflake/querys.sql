150a3978090d45a123b9d023f0dbe8b4b094c6a9
Logcomex <logcomex@LOG-MAC-00000336.local>
2025-04-09 21:21:03 -0300

    adding new columns

--- snowflake/querys.sql
+++ snowflake/querys.sql
@@ -2,17 +2,25 @@ DESC TABLE ecommerceonlineretail;
 
 -- Create a new products entity
 create or replace table products(
-	-- List the entity's attributes
-	stockcode VARCHAR(255),
+    -- List the entity's attributes
+    stockcode VARCHAR(255),
     description VARCHAR(255) 
 );
 
--- Create a new orders entity
-create or replace table orders(
-	-- List the invoice attributes
-	invoiceno VARCHAR(10),
+CREATE OR REPLACE TABLE orders (
+  	invoiceno VARCHAR(10) PRIMARY KEY,
   	invoicedate TIMESTAMP_NTZ(9),
-  	-- List the attributes related to price and quantity
   	unitprice NUMBER(10,2),
-  	quantity NUMBER(38)
+  	quantity NUMBER(38,0),
+  	-- Add columns that will refer the foreign key 
+	customerid NUMBER(38,0),
+	stockcode VARCHAR(255)
 );
+
+-- Create customers table 
+create or replace table customers (
+  -- Define unique identifier
+  customerid numeric(38,0) PRIMARY KEY,
+  country VARCHAR(255)
+);
+
