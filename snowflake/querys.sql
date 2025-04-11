5698e5055b60b69152d2c100b8b324f50a4b1e95
Logcomex <logcomex@LOG-MAC-00000336.local>
2025-04-10 21:14:29 -0300

    New querys about data quality and keys

--- snowflake/querys.sql
+++ snowflake/querys.sql
@@ -24,3 +24,45 @@ create or replace table customers (
   country VARCHAR(255)
 );
 
+CREATE OR REPLACE TABLE suppliers (
+
+    name VARCHAR(255),
+
+    location VARCHAR(255),
+
+    supplier_id NUMBER(38,0)
+
+);
+
+-- Alter suppliers table
+ALTER TABLE suppliers
+-- Add new column
+ADD COLUMN IF NOT EXISTS region VARCHAR(255);
+
+-- Alter suppliers table
+ALTER TABLE suppliers
+-- Add the new column
+ADD COLUMN IF NOT EXISTS contact VARCHAR(255);
+
+-- Alter suppliers table
+ALTER TABLE suppliers
+-- Assign the unique identifier
+ADD PRIMARY KEY (supplier_id);
+
+-- Create entity
+CREATE OR REPLACE TABLE batchdetails (
+	-- Add numerical attribute
+	batch_id NUMBER(10,0),
+	-- Add characters attributes
+    batch_number VARCHAR(255),
+    production_notes VARCHAR(255)
+);
+
+SELECT manufacturer, 
+	company_location, 
+	COUNT(*) AS product_count
+FROM productqualityrating
+GROUP BY manufacturer, 
+	company_location
+-- Add a filter for occurrence count greater than 1
+HAVING COUNT(*) >1;
